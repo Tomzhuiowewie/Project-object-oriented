f92cd7b6635480db47509a483074593c491c9fc2
Tomzhuiowewie <justdone_it@163.com>
2024-08-06 20:49:51 +0800

    README

---
+++ 01 全局区.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//全局变量
+int g_a = 10;
+int g_b = 10;
+
+int main() {
+
+	//全局区
+
+	//全局变量、静态变量、常量
+
+	//创建普通局部变量
+	int a = 10;
+	int b = 10;
+
+	cout << "局部变量a的地址为：" << (int)&a << endl;
+	cout << "局部变量b的地址为：" << (int)&b << endl;
+
+
+	system("pause");
+
+	return 0;
+}
