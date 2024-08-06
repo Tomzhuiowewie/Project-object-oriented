177e6493f936f5ccce38e3e8da47b7704f9ff8a6
Tomzhuiowewie <justdone_it@163.com>
2024-08-06 21:17:02 +0800

    01 全局区.cpp

--- 01 全局区.cpp
+++ 01 全局区.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int g_a = 10;
 int g_b = 10;
 
+const int c_g_a = 10;
+const int c_g_b = 10;
+
 int main() {
 
 	//全局区
@@ -20,6 +23,28 @@ int main() {
 
 	cout << "全局变量g_a的地址为：" << (int)&g_a << endl;
 	cout << "全局变量g_b的地址为：" << (int)&g_b << endl;
+
+	//静态变量 在普通变量前面加static,属于静态变量
+	static int s_a = 10;
+	static int s_b = 10;
+	cout << "静态变量s_a的地址为：" << (int)&s_a << endl;
+	cout << "静态变量s_b的地址为：" << (int)&s_b << endl;
+
+	//常量
+	//字符串常量
+	cout << "字符串常量的地址为：" << (int)&"hello world" << endl;
+
+	//const修饰的变量
+	//const修饰的全局变量，const修饰的局部变量
+
+	cout << "全局变量 c_g_a的地址为：" << (int)&c_g_a << endl;
+	cout << "全局变量 c_g_b的地址为：" << (int)&c_g_b << endl;
+
+	const int c_l_a = 10; // c-const g-global l-local
+	const int c_l_b = 10;
+
+	cout << "局部变量 c_l_a的地址为：" << (int)&c_l_a << endl;
+	cout << "局部变量 c_l_b的地址为：" << (int)&c_l_b << endl;
 
 	system("pause");
 
